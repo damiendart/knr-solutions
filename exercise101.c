9d7bad2de1cbd77e7620666aa0b906614dd7c23a
Damien Dart <damiendart@pobox.com>
2011-05-07 21:03:45 +0100

    Initial commit: Add solution to Exercise 1-1.

---
+++ exercise101.c
@@ -0,0 +1,12 @@
+/* Run the "hello, world" program on your system. Experiment with leaving out
+ * parts of the program, to see what error messages you get.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+  printf("Hello, world!\n");
+  return EXIT_SUCCESS;
+}
