d6b7b885b5cdd4ef12b2ba4f4b071dddbc092c4a
Damien Dart <damiendart@pobox.com>
2011-06-16 14:11:51 +0100

    Add solution to Exercise 1-12.

---
+++ exercise112.c
@@ -0,0 +1,24 @@
+/* Exercise 1-12: Write a program that prints its input one word per line. */
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+  int16_t character;
+  bool in_whitespace;
+  while ((character = getchar()) != EOF) {
+    if ((character == ' ') || (character == '\t')) {
+      if (in_whitespace == false) {
+        putchar('\n');
+        in_whitespace = true;
+      }
+    } else {
+      putchar(character);
+      in_whitespace = false;
+    }
+  }
+  return EXIT_SUCCESS;
+}
