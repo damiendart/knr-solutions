c8de8fae8e0f1a5ba0a0f0d6c3733807fe4ac552
Damien Dart <damiendart@pobox.com>
2011-10-06 22:56:50 +0100

    Add solution to Exercise 1-14.

---
+++ src/exercise114.c
@@ -0,0 +1,32 @@
+/* Exercise 1-14: Write a program to print a histogram of the frequencies of
+ * different characters in its input. */
+
+#include <limits.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+  uint32_t character_frequencies[CHAR_MAX] = { 0 };
+  int32_t character;
+  while ((character = getchar()) != EOF) {
+    /* Only count characters in the ASCII character set; life's too short. */
+    if (character < CHAR_MAX) {
+      character_frequencies[character]++;
+    }
+  }
+  /* Print a horizontally-aligned histogram. */
+  printf("ASCII Character Code | Frequency\n");
+  for (int32_t i = 0; i < CHAR_MAX + 1; i++) {
+    /* Don't display bars for characters that do not occur in input. */
+    if (character_frequencies[i] > 0) {
+      printf("%3d | ", i);
+      for (uint32_t j = 0; j < character_frequencies[i]; j++) {
+        printf("#");
+      }
+      printf(" (%d)\n", character_frequencies[i]);
+    }
+  }
+  return EXIT_SUCCESS;
+}
