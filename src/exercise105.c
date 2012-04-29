cee03732f413e538f3241d3c2a30721942a27f29
Damien Dart <damiendart@pobox.com>
2012-04-29 12:44:27 +0100

    Update file comments.

--- src/exercise105.c
+++ src/exercise105.c
@@ -1,7 +1,7 @@
 /*
  * A solution to Exercise 1-5 in The C Programming Language (Second Edition).
  *
- * This file was written by Damien Dart <damiendart@pobox.com>. This is free
+ * This file was written by Damien Dart, <damiendart@pobox.com>. This is free
  * and unencumbered software released into the public domain. For more
  * information, please refer to the accompanying "UNLICENCE" file.
  */
--- tests/exercise111.t
+++ tests/exercise111.t
@@ -2,7 +2,7 @@
 
 # Tests for Exercise 1-11 in The C Programming Language (Second Edition).
 #
-# This file was written by Damien Dart <damiendart@pobox.com>. This is free
+# This file was written by Damien Dart, <damiendart@pobox.com>. This is free
 # and unencumbered software released into the public domain. For more
 # information, please refer to the accompanying "UNLICENCE" file.
 
