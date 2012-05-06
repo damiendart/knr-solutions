8332b9d72c1f8c3003123cff09effbae9b414ab4
Damien Dart <damiendart@pobox.com>
2012-05-06 14:04:47 +0100

    Purge some tabs.

--- src/exercise205.c
+++ src/exercise205.c
@@ -14,19 +14,19 @@ int32_t any(char *, char *);
 
 int main(void)
 {
-	char *test_string_one = "Gooseberries.";
-	char *test_string_two = "Kiwis.";
-	printf("%d", any(test_string_one, test_string_two));
-	return EXIT_SUCCESS;
+  char *test_string_one = "Gooseberries.";
+  char *test_string_two = "Kiwis.";
+  printf("%d", any(test_string_one, test_string_two));
+  return EXIT_SUCCESS;
 }
 
 int32_t any(char *string_one, char *string_two)
 {
-	for (uint32_t i = 0; string_one[i] != '\0'; i++) {
-		for (uint32_t j = 0; string_two[j] != '\0'; j++) {
-			if (string_one[i] == string_two[j]) {
-				return i;
-			}
+  for (uint32_t i = 0; string_one[i] != '\0'; i++) {
+    for (uint32_t j = 0; string_two[j] != '\0'; j++) {
+      if (string_one[i] == string_two[j]) {
+        return i;
+      }
     }
   }
   return -1;
